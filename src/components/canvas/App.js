f2df0760a64d2b6b4e1aca611665e0d6ae341a56
mia napaswan chokechaikul <c.napaswan@gmail.com>
2019-12-16 05:58:14 +0700

    i broke my app by properly structuring it

---
+++ src/components/canvas/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import bubble from './bubble.svg';
+
+class Canvas extends React.Component {
+    render() {
+      return <img src={bubble} className="bubble" alt="logo" />;
+    }
+  }
+export default Canvas;
