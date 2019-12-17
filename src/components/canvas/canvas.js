fe2163acc737cdfd667ee0dfd67cfaab33c1f4b1
mia napaswan chokechaikul <c.napaswan@gmail.com>
2019-12-17 23:30:34 +0700

    styled a canvas to look more like a canvas

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Bob from '../bob/bob';
+import './style.css';
 
+//canvas is a stateless component to display bubbles
 class Canvas extends React.Component {
     render() {
-        return (<div><Bob/></div>);
+        return (<div className="canvas"><Bob/></div>);
     }
-  }
+}
+
+
 export default Canvas;
